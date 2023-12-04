0e923e6c346042bdd57273ee57eea7d4c4f8d4e4
Eric Udlis <udlis.eric@gmail.com>
2023-12-03 17:04:31 -0800

    format files with black formatter

--- inventree_digikey/__main__.py
+++ inventree_digikey/__main__.py
@@ -8,16 +8,33 @@ DEFAULT_CONFIG_PATH = Path(__file__).resolve().parent / "config.ini"
 
 
 def parse_args(args):
-    parser = argparse.ArgumentParser(description='Import Digikey part numbers into InvenTree')
+    parser = argparse.ArgumentParser(
+        description="Import Digikey part numbers into InvenTree"
+    )
 
     # Add an optional '-y' flag to bypass prompting
-    parser.add_argument('-y', "--yes", action='store_true', help='Bypass user prompts and assume "yes"', default=False)
-    parser.add_argument("-c", "--config", type=Path, help="Path to config file", default=DEFAULT_CONFIG_PATH)
+    parser.add_argument(
+        "-y",
+        "--yes",
+        action="store_true",
+        help='Bypass user prompts and assume "yes"',
+        default=False,
+    )
+    parser.add_argument(
+        "-c",
+        "--config",
+        type=Path,
+        help="Path to config file",
+        default=DEFAULT_CONFIG_PATH,
+    )
 
     # Add the 'part_number' argument as the last item on the command line
-    parser.add_argument('query_numbers', type=str, help='Part number(s) to import', nargs='+')
+    parser.add_argument(
+        "query_numbers", type=str, help="Part number(s) to import", nargs="+"
+    )
 
     return parser.parse_args(args)
+
 
 def main():
     args = parse_args(sys.argv[1:])
@@ -30,6 +47,7 @@ def main():
     else:
         for num in args.query_number:
             import_digikey_part(num, not args.yes)
+
 
 if __name__ == "__main__":
     main()
