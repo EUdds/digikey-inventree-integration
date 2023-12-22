447731cebd5e4526153d1c437fc01a2b525652a8
Eric Udlis <udlis@wisc.edu>
2023-12-22 13:58:47 -0800

    Fix typo in CLI, fix breaking config forgetfullness, add new test for this

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,4 +1,5 @@
 import inventree_digikey_integration.__main__ as test_module
+import inventree_digikey_integration.Inventree
 
 from pathlib import Path
 
@@ -45,3 +46,16 @@ def test_argparse():
     assert args.yes == True
     assert args.query_numbers == ["1234", "5678"]
     assert args.config == Path("test_config.ini")
+
+def test_import_parts(monkeypatch, test_data):
+    # This test is to make sure that the function chooses the correct branch of the if statement
+    # Lets ignore the actual parsing here
+    monkeypatch.setattr(test_module, "import_digikey_part", lambda x, y, z: None)
+    # Format (args, expected_return)
+    test_inputs = [
+        (["-c", f"{test_data['test_config_path']}", "4116R-1-151LF"], 1),
+    ]
+    for test_input in test_inputs:
+        args = test_module.parse_args(test_input[0])
+        res = test_module.import_parts(args)
+        assert res == test_input[1], "Expected return value of {} is {} but got {}".format(test_input[0], test_input[1], res)
